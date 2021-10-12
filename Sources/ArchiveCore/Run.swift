5eec722ecb324fe58505eb8c43088687644d31dc
NeilsUltimateLab <neilsultimatelab@icloud.com>
2021-10-12 06:14:32 +0530

    Added rey-load command.

--- Sources/ArchiveCore/Run.swift
+++ Sources/ArchiveCore/Run.swift
@@ -11,21 +11,30 @@ import Core
 import Uploader
 
 public struct Run: ParsableCommand, BuildInfoProvider {
+    public static var configuration: CommandConfiguration {
+        CommandConfiguration(subcommands: [Reupload.self])
+    }
+    
     public init() {}
     public func run() throws {
         print(">> Running the complete process -------")
         try Prepare().run()
         try Archive().run()
         try GenerateIPA().run()
-        try upload()
+        try Reupload().run()
     }
-    
-    func upload() throws {
-        print(">> Uploading to diawi -------")
-        let info = try self.buildInfo()
-        let path = try self.ipaPath()
-        let url = URL(fileURLWithPath: path)
-        let file = DiawiFile(url, fileName: url.lastPathComponent, token: info.diawiToken, emails: info.callbackEmails)
-        Uploader(file: file).run()
+}
+
+public extension Run {
+    struct Reupload: ParsableCommand, BuildInfoProvider {
+        public init() {}
+        public func run() throws {
+            print(">> Uploading to diawi -------")
+            let info = try self.buildInfo()
+            let path = try self.ipaPath()
+            let url = URL(fileURLWithPath: path)
+            let file = DiawiFile(url, fileName: url.lastPathComponent, token: info.diawiToken, emails: info.callbackEmails)
+            Uploader(file: file).run()
+        }
     }
 }
